31a425c772c5f821c51fe0651d7174900b6f1eb2
Paula Vasquez <avasquezc15@miumg.edu.gt>
2020-03-18 10:26:51 -0600

    Prueba1Paula

---
+++ RRHH/CapaDiseño/Procesos/SancionesDespidos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaDiseño.Procesos
+{
+    public partial class SancionesDespidos : UserControl
+    {
+        public SancionesDespidos()
+        {
+            InitializeComponent();
+        }
+    }
+}
