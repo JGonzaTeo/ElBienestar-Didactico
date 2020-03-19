ceee157fda4398727be3f0d31610b976d835fa18
Paula Vasquez <avasquezc15@miumg.edu.gt>
2020-03-18 19:30:30 -0600

    Formularios

---
+++ HRM/RRHH/CapaDiseño/Mantenimientos/Frm_MantIngresoReunion.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace CapaDiseño.Mantenimientos
+{
+    partial class Frm_MantIngresoReunion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_MantIngresoReunion));
+            this.Btn_Guardar = new System.Windows.Forms.Button();
+            this.Txt_CantidadEmpleados = new System.Windows.Forms.TextBox();
+            this.Dtp_FechaFinalizacion = new System.Windows.Forms.DateTimePicker();
+            this.Dtp_FechaInicio = new System.Windows.Forms.DateTimePicker();
+            this.Txt_DescripcionReunion = new System.Windows.Forms.TextBox();
+            this.Txt_NombreReunion = new System.Windows.Forms.TextBox();
+            this.Lbl_CantidadEmpleados = new System.Windows.Forms.Label();
+            this.Lbl_HoraFinalizacion = new System.Windows.Forms.Label();
+            this.Lbl_FechaFinalizacion = new System.Windows.Forms.Label();
+            this.Lbl_HoraInicio = new System.Windows.Forms.Label();
+            this.Lbl_FechaInicio = new System.Windows.Forms.Label();
+            this.LblDescripcion = new System.Windows.Forms.Label();
+            this.Lbl_NombreReunion = new System.Windows.Forms.Label();
+            this.Pic_Cerrar = new System.Windows.Forms.PictureBox();
+            this.Pic_Minimizar = new System.Windows.Forms.PictureBox();
+            this.Pic_Pregunta = new System.Windows.Forms.PictureBox();
+            this.Pic_Barra = new System.Windows.Forms.PictureBox();
+            this.Lbl_CreacionReunion = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_Cerrar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_Minimizar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_Pregunta)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_Barra)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Btn_Guardar
+            // 
+            this.Btn_Guardar.Location = new System.Drawing.Point(463, 260);
+            this.Btn_Guardar.Name = "Btn_Guardar";
+            this.Btn_Guardar.Size = new System.Drawing.Size(93, 30);
+            this.Btn_Guardar.TabIndex = 59;
+            this.Btn_Guardar.Text = "Guardar";
+            this.Btn_Guardar.UseVisualStyleBackColor = true;
+            // 
+            // Txt_CantidadEmpleados
+            // 
+            this.Txt_CantidadEmpleados.Location = new System.Drawing.Point(183, 268);
+            this.Txt_CantidadEmpleados.Name = "Txt_CantidadEmpleados";
+            this.Txt_CantidadEmpleados.Size = new System.Drawing.Size(105, 22);
+            this.Txt_CantidadEmpleados.TabIndex = 58;
+            // 
+            // Dtp_FechaFinalizacion
+            // 
+            this.Dtp_FechaFinalizacion.Location = new System.Drawing.Point(174, 212);
+            this.Dtp_FechaFinalizacion.Name = "Dtp_FechaFinalizacion";
+            this.Dtp_FechaFinalizacion.Size = new System.Drawing.Size(222, 22);
+            this.Dtp_FechaFinalizacion.TabIndex = 57;
+            // 
+            // Dtp_FechaInicio
+            // 
+            this.Dtp_FechaInicio.Location = new System.Drawing.Point(158, 160);
+            this.Dtp_FechaInicio.Name = "Dtp_FechaInicio";
+            this.Dtp_FechaInicio.Size = new System.Drawing.Size(216, 22);
+            this.Dtp_FechaInicio.TabIndex = 56;
+            // 
+            // Txt_DescripcionReunion
+            // 
+            this.Txt_DescripcionReunion.Location = new System.Drawing.Point(145, 115);
+            this.Txt_DescripcionReunion.Name = "Txt_DescripcionReunion";
+            this.Txt_DescripcionReunion.Size = new System.Drawing.Size(420, 22);
+            this.Txt_DescripcionReunion.TabIndex = 55;
+            // 
+            // Txt_NombreReunion
+            // 
+            this.Txt_NombreReunion.Location = new System.Drawing.Point(195, 79);
+            this.Txt_NombreReunion.Name = "Txt_NombreReunion";
+            this.Txt_NombreReunion.Size = new System.Drawing.Size(370, 22);
+            this.Txt_NombreReunion.TabIndex = 54;
+            // 
+            // Lbl_CantidadEmpleados
+            // 
+            this.Lbl_CantidadEmpleados.AutoSize = true;
+            this.Lbl_CantidadEmpleados.Location = new System.Drawing.Point(36, 268);
+            this.Lbl_CantidadEmpleados.Name = "Lbl_CantidadEmpleados";
+            this.Lbl_CantidadEmpleados.Size = new System.Drawing.Size(138, 17);
+            this.Lbl_CantidadEmpleados.TabIndex = 53;
+            this.Lbl_CantidadEmpleados.Text = "Cantidad Empleados";
+            // 
+            // Lbl_HoraFinalizacion
+            // 
+            this.Lbl_HoraFinalizacion.AutoSize = true;
+            this.Lbl_HoraFinalizacion.Location = new System.Drawing.Point(36, 232);
+            this.Lbl_HoraFinalizacion.Name = "Lbl_HoraFinalizacion";
+            this.Lbl_HoraFinalizacion.Size = new System.Drawing.Size(137, 17);
+            this.Lbl_HoraFinalizacion.TabIndex = 52;
+            this.Lbl_HoraFinalizacion.Text = "Hora de Finalización";
+            // 
+            // Lbl_FechaFinalizacion
+            // 
+            this.Lbl_FechaFinalizacion.AutoSize = true;
+            this.Lbl_FechaFinalizacion.Location = new System.Drawing.Point(36, 214);
+            this.Lbl_FechaFinalizacion.Name = "Lbl_FechaFinalizacion";
+            this.Lbl_FechaFinalizacion.Size = new System.Drawing.Size(125, 17);
+            this.Lbl_FechaFinalizacion.TabIndex = 51;
+            this.Lbl_FechaFinalizacion.Text = "Fecha Finalización";
+            // 
+            // Lbl_HoraInicio
+            // 
+            this.Lbl_HoraInicio.AutoSize = true;
+            this.Lbl_HoraInicio.Location = new System.Drawing.Point(39, 185);
+            this.Lbl_HoraInicio.Name = "Lbl_HoraInicio";
+            this.Lbl_HoraInicio.Size = new System.Drawing.Size(95, 17);
+            this.Lbl_HoraInicio.TabIndex = 50;
+            this.Lbl_HoraInicio.Text = "Hora de Inicio";
+            // 
+            // Lbl_FechaInicio
+            // 
+            this.Lbl_FechaInicio.AutoSize = true;
+            this.Lbl_FechaInicio.Location = new System.Drawing.Point(35, 163);
+            this.Lbl_FechaInicio.Name = "Lbl_FechaInicio";
+            this.Lbl_FechaInicio.Size = new System.Drawing.Size(103, 17);
+            this.Lbl_FechaInicio.TabIndex = 49;
+            this.Lbl_FechaInicio.Text = "Fecha de Inicio";
+            // 
+            // LblDescripcion
+            // 
+            this.LblDescripcion.AutoSize = true;
+            this.LblDescripcion.Location = new System.Drawing.Point(39, 115);
+            this.LblDescripcion.Name = "LblDescripcion";
+            this.LblDescripcion.Size = new System.Drawing.Size(82, 17);
+            this.LblDescripcion.TabIndex = 48;
+            this.LblDescripcion.Text = "Descripción";
+            // 
+            // Lbl_NombreReunion
+            // 
+            this.Lbl_NombreReunion.AutoSize = true;
+            this.Lbl_NombreReunion.Location = new System.Drawing.Point(39, 79);
+            this.Lbl_NombreReunion.Name = "Lbl_NombreReunion";
+            this.Lbl_NombreReunion.Size = new System.Drawing.Size(150, 17);
+            this.Lbl_NombreReunion.TabIndex = 47;
+            this.Lbl_NombreReunion.Text = "Nombre de la Reunión";
+            // 
+            // Pic_Cerrar
+            // 
+            this.Pic_Cerrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(107)))), ((int)(((byte)(44)))));
+            this.Pic_Cerrar.Image = ((System.Drawing.Image)(resources.GetObject("Pic_Cerrar.Image")));
+            this.Pic_Cerrar.Location = new System.Drawing.Point(571, 9);
+            this.Pic_Cerrar.Name = "Pic_Cerrar";
+            this.Pic_Cerrar.Size = new System.Drawing.Size(24, 23);
+            this.Pic_Cerrar.TabIndex = 64;
+            this.Pic_Cerrar.TabStop = false;
+            // 
+            // Pic_Minimizar
+            // 
+            this.Pic_Minimizar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(107)))), ((int)(((byte)(44)))));
+            this.Pic_Minimizar.Image = ((System.Drawing.Image)(resources.GetObject("Pic_Minimizar.Image")));
+            this.Pic_Minimizar.Location = new System.Drawing.Point(508, 7);
+            this.Pic_Minimizar.Name = "Pic_Minimizar";
+            this.Pic_Minimizar.Size = new System.Drawing.Size(26, 26);
+            this.Pic_Minimizar.TabIndex = 63;
+            this.Pic_Minimizar.TabStop = false;
+            // 
+            // Pic_Pregunta
+            // 
+            this.Pic_Pregunta.Image = ((System.Drawing.Image)(resources.GetObject("Pic_Pregunta.Image")));
+            this.Pic_Pregunta.InitialImage = ((System.Drawing.Image)(resources.GetObject("Pic_Pregunta.InitialImage")));
+            this.Pic_Pregunta.Location = new System.Drawing.Point(541, 7);
+            this.Pic_Pregunta.Name = "Pic_Pregunta";
+            this.Pic_Pregunta.Size = new System.Drawing.Size(23, 26);
+            this.Pic_Pregunta.TabIndex = 62;
+            this.Pic_Pregunta.TabStop = false;
+            // 
+            // Pic_Barra
+            // 
+            this.Pic_Barra.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(107)))), ((int)(((byte)(44)))));
+            this.Pic_Barra.Location = new System.Drawing.Point(-8, -1);
+            this.Pic_Barra.Name = "Pic_Barra";
+            this.Pic_Barra.Size = new System.Drawing.Size(615, 37);
+            this.Pic_Barra.TabIndex = 61;
+            this.Pic_Barra.TabStop = false;
+            // 
+            // Lbl_CreacionReunion
+            // 
+            this.Lbl_CreacionReunion.AutoSize = true;
+            this.Lbl_CreacionReunion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(107)))), ((int)(((byte)(44)))));
+            this.Lbl_CreacionReunion.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_CreacionReunion.ForeColor = System.Drawing.Color.White;
+            this.Lbl_CreacionReunion.Location = new System.Drawing.Point(12, 2);
+            this.Lbl_CreacionReunion.Name = "Lbl_CreacionReunion";
+            this.Lbl_CreacionReunion.Size = new System.Drawing.Size(287, 31);
+            this.Lbl_CreacionReunion.TabIndex = 65;
+            this.Lbl_CreacionReunion.Text = "Creación de Reunión";
+            // 
+            // Frm_MantIngresoReunion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 342);
+            this.Controls.Add(this.Lbl_CreacionReunion);
+            this.Controls.Add(this.Pic_Cerrar);
+            this.Controls.Add(this.Pic_Minimizar);
+            this.Controls.Add(this.Pic_Pregunta);
+            this.Controls.Add(this.Pic_Barra);
+            this.Controls.Add(this.Btn_Guardar);
+            this.Controls.Add(this.Txt_CantidadEmpleados);
+            this.Controls.Add(this.Dtp_FechaFinalizacion);
+            this.Controls.Add(this.Dtp_FechaInicio);
+            this.Controls.Add(this.Txt_DescripcionReunion);
+            this.Controls.Add(this.Txt_NombreReunion);
+            this.Controls.Add(this.Lbl_CantidadEmpleados);
+            this.Controls.Add(this.Lbl_HoraFinalizacion);
+            this.Controls.Add(this.Lbl_FechaFinalizacion);
+            this.Controls.Add(this.Lbl_HoraInicio);
+            this.Controls.Add(this.Lbl_FechaInicio);
+            this.Controls.Add(this.LblDescripcion);
+            this.Controls.Add(this.Lbl_NombreReunion);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Frm_MantIngresoReunion";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Frm_MantIngresoReunion";
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_Cerrar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_Minimizar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_Pregunta)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_Barra)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_Guardar;
+        private System.Windows.Forms.TextBox Txt_CantidadEmpleados;
+        private System.Windows.Forms.DateTimePicker Dtp_FechaFinalizacion;
+        private System.Windows.Forms.DateTimePicker Dtp_FechaInicio;
+        private System.Windows.Forms.TextBox Txt_DescripcionReunion;
+        private System.Windows.Forms.TextBox Txt_NombreReunion;
+        private System.Windows.Forms.Label Lbl_CantidadEmpleados;
+        private System.Windows.Forms.Label Lbl_HoraFinalizacion;
+        private System.Windows.Forms.Label Lbl_FechaFinalizacion;
+        private System.Windows.Forms.Label Lbl_HoraInicio;
+        private System.Windows.Forms.Label Lbl_FechaInicio;
+        private System.Windows.Forms.Label LblDescripcion;
+        private System.Windows.Forms.Label Lbl_NombreReunion;
+        private System.Windows.Forms.PictureBox Pic_Cerrar;
+        private System.Windows.Forms.PictureBox Pic_Minimizar;
+        private System.Windows.Forms.PictureBox Pic_Pregunta;
+        private System.Windows.Forms.PictureBox Pic_Barra;
+        private System.Windows.Forms.Label Lbl_CreacionReunion;
+    }
+}
