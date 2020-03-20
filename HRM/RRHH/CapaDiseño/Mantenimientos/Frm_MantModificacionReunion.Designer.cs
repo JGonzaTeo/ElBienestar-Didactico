92d575ed6f761dc9bc602a117643b75dcf64df6b
Paula Vasquez <avasquezc15@miumg.edu.gt>
2020-03-20 11:37:22 -0600

    VersionPaula

---
+++ HRM/RRHH/CapaDiseño/Mantenimientos/Frm_MantModificacionReunion.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace CapaDiseño.Mantenimientos
+{
+    partial class Frm_MantModificacionReunion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_MantModificacionReunion));
+            this.Pic_Barra = new System.Windows.Forms.PictureBox();
+            this.Lbl_ModificacionReunion = new System.Windows.Forms.Label();
+            this.Btn_Guardar = new System.Windows.Forms.Button();
+            this.Gpb_DatosReunion = new System.Windows.Forms.GroupBox();
+            this.Txt_HoraFinal = new System.Windows.Forms.TextBox();
+            this.Txt_FechaFinal = new System.Windows.Forms.TextBox();
+            this.Txt_HoraInicio = new System.Windows.Forms.TextBox();
+            this.Txt_FechaInicio = new System.Windows.Forms.TextBox();
+            this.Lbl_HoraFinalizacion = new System.Windows.Forms.Label();
+            this.Lbl_FechaFinalizacion = new System.Windows.Forms.Label();
+            this.Lbl_HoraInicio = new System.Windows.Forms.Label();
+            this.Lbl_FechaInicio = new System.Windows.Forms.Label();
+            this.Txt_CantidadEmpleado = new System.Windows.Forms.TextBox();
+            this.Txt_Descripcion = new System.Windows.Forms.TextBox();
+            this.Lbl_CantidadEmpleados = new System.Windows.Forms.Label();
+            this.Lbl_Descripcion = new System.Windows.Forms.Label();
+            this.Gpb_Reunion = new System.Windows.Forms.GroupBox();
+            this.Btn_Buscar = new System.Windows.Forms.Button();
+            this.Txt_NombreReunion = new System.Windows.Forms.TextBox();
+            this.Lbl_NombreReunion = new System.Windows.Forms.Label();
+            this.btn_Ayuda = new System.Windows.Forms.Button();
+            this.btn_minimizar = new System.Windows.Forms.Button();
+            this.btn_cerrar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_Barra)).BeginInit();
+            this.Gpb_DatosReunion.SuspendLayout();
+            this.Gpb_Reunion.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Pic_Barra
+            // 
+            this.Pic_Barra.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(107)))), ((int)(((byte)(44)))));
+            this.Pic_Barra.Location = new System.Drawing.Point(-3, -1);
+            this.Pic_Barra.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Pic_Barra.Name = "Pic_Barra";
+            this.Pic_Barra.Size = new System.Drawing.Size(635, 54);
+            this.Pic_Barra.TabIndex = 60;
+            this.Pic_Barra.TabStop = false;
+            // 
+            // Lbl_ModificacionReunion
+            // 
+            this.Lbl_ModificacionReunion.AutoSize = true;
+            this.Lbl_ModificacionReunion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(107)))), ((int)(((byte)(44)))));
+            this.Lbl_ModificacionReunion.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_ModificacionReunion.ForeColor = System.Drawing.Color.White;
+            this.Lbl_ModificacionReunion.Location = new System.Drawing.Point(8, 11);
+            this.Lbl_ModificacionReunion.Name = "Lbl_ModificacionReunion";
+            this.Lbl_ModificacionReunion.Size = new System.Drawing.Size(364, 31);
+            this.Lbl_ModificacionReunion.TabIndex = 64;
+            this.Lbl_ModificacionReunion.Text = "Modificaciones en Reunión";
+            // 
+            // Btn_Guardar
+            // 
+            this.Btn_Guardar.Location = new System.Drawing.Point(261, 417);
+            this.Btn_Guardar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Btn_Guardar.Name = "Btn_Guardar";
+            this.Btn_Guardar.Size = new System.Drawing.Size(120, 39);
+            this.Btn_Guardar.TabIndex = 65;
+            this.Btn_Guardar.Text = "Guardar";
+            this.Btn_Guardar.UseVisualStyleBackColor = true;
+            this.Btn_Guardar.Click += new System.EventHandler(this.Btn_Guardar_Click);
+            // 
+            // Gpb_DatosReunion
+            // 
+            this.Gpb_DatosReunion.Controls.Add(this.Txt_HoraFinal);
+            this.Gpb_DatosReunion.Controls.Add(this.Txt_FechaFinal);
+            this.Gpb_DatosReunion.Controls.Add(this.Txt_HoraInicio);
+            this.Gpb_DatosReunion.Controls.Add(this.Txt_FechaInicio);
+            this.Gpb_DatosReunion.Controls.Add(this.Lbl_HoraFinalizacion);
+            this.Gpb_DatosReunion.Controls.Add(this.Lbl_FechaFinalizacion);
+            this.Gpb_DatosReunion.Controls.Add(this.Lbl_HoraInicio);
+            this.Gpb_DatosReunion.Controls.Add(this.Lbl_FechaInicio);
+            this.Gpb_DatosReunion.Controls.Add(this.Txt_CantidadEmpleado);
+            this.Gpb_DatosReunion.Controls.Add(this.Txt_Descripcion);
+            this.Gpb_DatosReunion.Controls.Add(this.Lbl_CantidadEmpleados);
+            this.Gpb_DatosReunion.Controls.Add(this.Lbl_Descripcion);
+            this.Gpb_DatosReunion.Location = new System.Drawing.Point(15, 176);
+            this.Gpb_DatosReunion.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Gpb_DatosReunion.Name = "Gpb_DatosReunion";
+            this.Gpb_DatosReunion.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Gpb_DatosReunion.Size = new System.Drawing.Size(592, 225);
+            this.Gpb_DatosReunion.TabIndex = 67;
+            this.Gpb_DatosReunion.TabStop = false;
+            this.Gpb_DatosReunion.Text = "Datos de la Reunión";
+            // 
+            // Txt_HoraFinal
+            // 
+            this.Txt_HoraFinal.Location = new System.Drawing.Point(428, 137);
+            this.Txt_HoraFinal.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Txt_HoraFinal.Name = "Txt_HoraFinal";
+            this.Txt_HoraFinal.Size = new System.Drawing.Size(129, 22);
+            this.Txt_HoraFinal.TabIndex = 81;
+            // 
+            // Txt_FechaFinal
+            // 
+            this.Txt_FechaFinal.Location = new System.Drawing.Point(137, 132);
+            this.Txt_FechaFinal.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Txt_FechaFinal.Name = "Txt_FechaFinal";
+            this.Txt_FechaFinal.Size = new System.Drawing.Size(169, 22);
+            this.Txt_FechaFinal.TabIndex = 80;
+            // 
+            // Txt_HoraInicio
+            // 
+            this.Txt_HoraInicio.Location = new System.Drawing.Point(428, 87);
+            this.Txt_HoraInicio.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Txt_HoraInicio.Name = "Txt_HoraInicio";
+            this.Txt_HoraInicio.Size = new System.Drawing.Size(129, 22);
+            this.Txt_HoraInicio.TabIndex = 79;
+            // 
+            // Txt_FechaInicio
+            // 
+            this.Txt_FechaInicio.Location = new System.Drawing.Point(137, 87);
+            this.Txt_FechaInicio.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Txt_FechaInicio.Name = "Txt_FechaInicio";
+            this.Txt_FechaInicio.Size = new System.Drawing.Size(169, 22);
+            this.Txt_FechaInicio.TabIndex = 78;
+            // 
+            // Lbl_HoraFinalizacion
+            // 
+            this.Lbl_HoraFinalizacion.AutoSize = true;
+            this.Lbl_HoraFinalizacion.Location = new System.Drawing.Point(327, 134);
+            this.Lbl_HoraFinalizacion.Name = "Lbl_HoraFinalizacion";
+            this.Lbl_HoraFinalizacion.Size = new System.Drawing.Size(73, 17);
+            this.Lbl_HoraFinalizacion.TabIndex = 77;
+            this.Lbl_HoraFinalizacion.Text = "Hora Final";
+            // 
+            // Lbl_FechaFinalizacion
+            // 
+            this.Lbl_FechaFinalizacion.AutoSize = true;
+            this.Lbl_FechaFinalizacion.Location = new System.Drawing.Point(27, 134);
+            this.Lbl_FechaFinalizacion.Name = "Lbl_FechaFinalizacion";
+            this.Lbl_FechaFinalizacion.Size = new System.Drawing.Size(81, 17);
+            this.Lbl_FechaFinalizacion.TabIndex = 76;
+            this.Lbl_FechaFinalizacion.Text = "Fecha Final";
+            // 
+            // Lbl_HoraInicio
+            // 
+            this.Lbl_HoraInicio.AutoSize = true;
+            this.Lbl_HoraInicio.Location = new System.Drawing.Point(327, 90);
+            this.Lbl_HoraInicio.Name = "Lbl_HoraInicio";
+            this.Lbl_HoraInicio.Size = new System.Drawing.Size(95, 17);
+            this.Lbl_HoraInicio.TabIndex = 75;
+            this.Lbl_HoraInicio.Text = "Hora de Inicio";
+            // 
+            // Lbl_FechaInicio
+            // 
+            this.Lbl_FechaInicio.AutoSize = true;
+            this.Lbl_FechaInicio.Location = new System.Drawing.Point(27, 90);
+            this.Lbl_FechaInicio.Name = "Lbl_FechaInicio";
+            this.Lbl_FechaInicio.Size = new System.Drawing.Size(103, 17);
+            this.Lbl_FechaInicio.TabIndex = 74;
+            this.Lbl_FechaInicio.Text = "Fecha de Inicio";
+            // 
+            // Txt_CantidadEmpleado
+            // 
+            this.Txt_CantidadEmpleado.Location = new System.Drawing.Point(168, 172);
+            this.Txt_CantidadEmpleado.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Txt_CantidadEmpleado.Name = "Txt_CantidadEmpleado";
+            this.Txt_CantidadEmpleado.Size = new System.Drawing.Size(139, 22);
+            this.Txt_CantidadEmpleado.TabIndex = 43;
+            // 
+            // Txt_Descripcion
+            // 
+            this.Txt_Descripcion.Location = new System.Drawing.Point(139, 36);
+            this.Txt_Descripcion.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Txt_Descripcion.Name = "Txt_Descripcion";
+            this.Txt_Descripcion.Size = new System.Drawing.Size(420, 22);
+            this.Txt_Descripcion.TabIndex = 40;
+            // 
+            // Lbl_CantidadEmpleados
+            // 
+            this.Lbl_CantidadEmpleados.AutoSize = true;
+            this.Lbl_CantidadEmpleados.Location = new System.Drawing.Point(21, 175);
+            this.Lbl_CantidadEmpleados.Name = "Lbl_CantidadEmpleados";
+            this.Lbl_CantidadEmpleados.Size = new System.Drawing.Size(138, 17);
+            this.Lbl_CantidadEmpleados.TabIndex = 38;
+            this.Lbl_CantidadEmpleados.Text = "Cantidad Empleados";
+            // 
+            // Lbl_Descripcion
+            // 
+            this.Lbl_Descripcion.AutoSize = true;
+            this.Lbl_Descripcion.Location = new System.Drawing.Point(33, 38);
+            this.Lbl_Descripcion.Name = "Lbl_Descripcion";
+            this.Lbl_Descripcion.Size = new System.Drawing.Size(82, 17);
+            this.Lbl_Descripcion.TabIndex = 33;
+            this.Lbl_Descripcion.Text = "Descripción";
+            // 
+            // Gpb_Reunion
+            // 
+            this.Gpb_Reunion.Controls.Add(this.Btn_Buscar);
+            this.Gpb_Reunion.Controls.Add(this.Txt_NombreReunion);
+            this.Gpb_Reunion.Controls.Add(this.Lbl_NombreReunion);
+            this.Gpb_Reunion.Location = new System.Drawing.Point(15, 84);
+            this.Gpb_Reunion.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Gpb_Reunion.Name = "Gpb_Reunion";
+            this.Gpb_Reunion.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Gpb_Reunion.Size = new System.Drawing.Size(592, 73);
+            this.Gpb_Reunion.TabIndex = 66;
+            this.Gpb_Reunion.TabStop = false;
+            this.Gpb_Reunion.Text = "Reunión";
+            // 
+            // Btn_Buscar
+            // 
+            this.Btn_Buscar.Location = new System.Drawing.Point(464, 21);
+            this.Btn_Buscar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Btn_Buscar.Name = "Btn_Buscar";
+            this.Btn_Buscar.Size = new System.Drawing.Size(96, 36);
+            this.Btn_Buscar.TabIndex = 47;
+            this.Btn_Buscar.Text = "Buscar";
+            this.Btn_Buscar.UseVisualStyleBackColor = true;
+            this.Btn_Buscar.Click += new System.EventHandler(this.Btn_Buscar_Click);
+            // 
+            // Txt_NombreReunion
+            // 
+            this.Txt_NombreReunion.Location = new System.Drawing.Point(177, 31);
+            this.Txt_NombreReunion.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Txt_NombreReunion.Name = "Txt_NombreReunion";
+            this.Txt_NombreReunion.Size = new System.Drawing.Size(263, 22);
+            this.Txt_NombreReunion.TabIndex = 39;
+            // 
+            // Lbl_NombreReunion
+            // 
+            this.Lbl_NombreReunion.AutoSize = true;
+            this.Lbl_NombreReunion.Location = new System.Drawing.Point(21, 34);
+            this.Lbl_NombreReunion.Name = "Lbl_NombreReunion";
+            this.Lbl_NombreReunion.Size = new System.Drawing.Size(150, 17);
+            this.Lbl_NombreReunion.TabIndex = 32;
+            this.Lbl_NombreReunion.Text = "Nombre de la Reunión";
+            // 
+            // btn_Ayuda
+            // 
+            this.btn_Ayuda.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(107)))), ((int)(((byte)(44)))));
+            this.btn_Ayuda.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_Ayuda.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Ayuda.Image = global::CapaDiseño.Properties.Resources.question__1_;
+            this.btn_Ayuda.Location = new System.Drawing.Point(521, 5);
+            this.btn_Ayuda.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_Ayuda.Name = "btn_Ayuda";
+            this.btn_Ayuda.Size = new System.Drawing.Size(44, 38);
+            this.btn_Ayuda.TabIndex = 70;
+            this.btn_Ayuda.UseVisualStyleBackColor = false;
+            this.btn_Ayuda.Click += new System.EventHandler(this.btn_Ayuda_Click);
+            // 
+            // btn_minimizar
+            // 
+            this.btn_minimizar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(107)))), ((int)(((byte)(44)))));
+            this.btn_minimizar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_minimizar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_minimizar.Image = ((System.Drawing.Image)(resources.GetObject("btn_minimizar.Image")));
+            this.btn_minimizar.Location = new System.Drawing.Point(472, 5);
+            this.btn_minimizar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_minimizar.Name = "btn_minimizar";
+            this.btn_minimizar.Size = new System.Drawing.Size(44, 38);
+            this.btn_minimizar.TabIndex = 69;
+            this.btn_minimizar.UseVisualStyleBackColor = false;
+            this.btn_minimizar.Click += new System.EventHandler(this.btn_minimizar_Click);
+            // 
+            // btn_cerrar
+            // 
+            this.btn_cerrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(107)))), ((int)(((byte)(44)))));
+            this.btn_cerrar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_cerrar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_cerrar.Image = ((System.Drawing.Image)(resources.GetObject("btn_cerrar.Image")));
+            this.btn_cerrar.Location = new System.Drawing.Point(571, 5);
+            this.btn_cerrar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_cerrar.Name = "btn_cerrar";
+            this.btn_cerrar.Size = new System.Drawing.Size(44, 38);
+            this.btn_cerrar.TabIndex = 68;
+            this.btn_cerrar.UseVisualStyleBackColor = false;
+            this.btn_cerrar.Click += new System.EventHandler(this.btn_cerrar_Click);
+            // 
+            // Frm_MantModificacionReunion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 482);
+            this.Controls.Add(this.btn_Ayuda);
+            this.Controls.Add(this.btn_minimizar);
+            this.Controls.Add(this.btn_cerrar);
+            this.Controls.Add(this.Btn_Guardar);
+            this.Controls.Add(this.Gpb_DatosReunion);
+            this.Controls.Add(this.Gpb_Reunion);
+            this.Controls.Add(this.Lbl_ModificacionReunion);
+            this.Controls.Add(this.Pic_Barra);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Frm_MantModificacionReunion";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Frm_MantModificacionReunion";
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_Barra)).EndInit();
+            this.Gpb_DatosReunion.ResumeLayout(false);
+            this.Gpb_DatosReunion.PerformLayout();
+            this.Gpb_Reunion.ResumeLayout(false);
+            this.Gpb_Reunion.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox Pic_Barra;
+        private System.Windows.Forms.Label Lbl_ModificacionReunion;
+        private System.Windows.Forms.Button Btn_Guardar;
+        private System.Windows.Forms.GroupBox Gpb_DatosReunion;
+        private System.Windows.Forms.TextBox Txt_HoraFinal;
+        private System.Windows.Forms.TextBox Txt_FechaFinal;
+        private System.Windows.Forms.TextBox Txt_HoraInicio;
+        private System.Windows.Forms.TextBox Txt_FechaInicio;
+        private System.Windows.Forms.Label Lbl_HoraFinalizacion;
+        private System.Windows.Forms.Label Lbl_FechaFinalizacion;
+        private System.Windows.Forms.Label Lbl_HoraInicio;
+        private System.Windows.Forms.Label Lbl_FechaInicio;
+        private System.Windows.Forms.TextBox Txt_CantidadEmpleado;
+        private System.Windows.Forms.TextBox Txt_Descripcion;
+        private System.Windows.Forms.Label Lbl_CantidadEmpleados;
+        private System.Windows.Forms.Label Lbl_Descripcion;
+        private System.Windows.Forms.GroupBox Gpb_Reunion;
+        private System.Windows.Forms.Button Btn_Buscar;
+        private System.Windows.Forms.TextBox Txt_NombreReunion;
+        private System.Windows.Forms.Label Lbl_NombreReunion;
+        private System.Windows.Forms.Button btn_Ayuda;
+        private System.Windows.Forms.Button btn_minimizar;
+        private System.Windows.Forms.Button btn_cerrar;
+    }
+}
---
+++ HRM/RRHH/CapaDiseño/Mantenimientos/Frm_MantPerfiles.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Odbc;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaDiseño.Consulta;
+using CapaLogica;
+
+namespace CapaDiseño.Mantenimientos
+{
+    public partial class Frm_MantPerfiles : Form
+    {
+        Logica logic = new Logica();
+        string scampo;
+        public Frm_MantPerfiles()
+        {
+            InitializeComponent();
+            scampo = logic.siguiente("perfil_encabezado", "pkcodperfil");
+            Txt_Cod.Text = scampo;
+            chc_primaria.Enabled = false;
+            chc_secundaria.Enabled = false;
+            chc_bachillerato.Enabled = false;
+            chc_Estudiante.Enabled = false;
+            chc_Graduado.Enabled = false;
+            chc_Cursos.Enabled = false;
+            txt_extras.Enabled = false;
+            Txt_Cod.Enabled = false;
+            Txt_puesto.Enabled = false;
+        }
+
+        private void btn_buscarCreador_Click(object sender, EventArgs e)
+        {
+            Frm_consultaEmpleado concep = new Frm_consultaEmpleado();
+            concep.ShowDialog();
+
+            if (concep.DialogResult == DialogResult.OK)
+            {
+                Txt_puesto.Text = concep.Dgv_consultaEmpleado.Rows[concep.Dgv_consultaEmpleado.CurrentRow.Index].
+                      Cells[0].Value.ToString();
+            }
+            if (string.IsNullOrEmpty(Txt_puesto.Text))
+                MessageBox.Show("Debe de seleccionar un puesto");
+            
+        }
+
+        private void Btn_ingresar_Click(object sender, EventArgs e)
+        {
+            chc_primaria.Enabled = true;
+            chc_secundaria.Enabled = true;
+            chc_bachillerato.Enabled = true;
+            chc_Estudiante.Enabled = true;
+            chc_Graduado.Enabled = true;
+            chc_Cursos.Enabled = true;
+            txt_extras.Enabled = true;
+            Txt_Cod.Enabled = true;
+            Txt_puesto.Enabled = true;
+        }
+
+        private void btn_minimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btn_cerrar_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void Btn_consultar_Click(object sender, EventArgs e)
+        {
+            Frm_consltaPerfiles perfil = new Frm_consltaPerfiles();
+            perfil.ShowDialog();
+
+            if (perfil.DialogResult == DialogResult.OK)
+            {
+                Txt_Cod.Text = perfil.Dgv_consultaPerfil.Rows[perfil.Dgv_consultaPerfil.CurrentRow.Index].
+                      Cells[0].Value.ToString();
+
+            }
+        }
+
+        private void Btn_guardar_Click(object sender, EventArgs e)
+        {
+            //Conversion a numeros
+            bool p, s, b, es, g, c;
+            p = chc_primaria.Checked;
+            s = chc_secundaria.Checked;
+            b = chc_bachillerato.Checked;
+            es = chc_Estudiante.Checked;
+            g = chc_Graduado.Checked;
+            c = chc_Cursos.Checked;
+
+            string p2, s2, b2, es2, g2, c2;
+            int p1, s1, b1, es1, g1, c1;
+
+            p1 = Convert.ToInt32(p);
+            s1 = Convert.ToInt32(s);
+            b1 = Convert.ToInt32(b);
+            es1 = Convert.ToInt32(es);
+            g1 = Convert.ToInt32(g);
+            c1 = Convert.ToInt32(c);
+
+            p2 = p1.ToString();
+            s2 = s1.ToString();
+            b2 = b1.ToString();
+            es2 = es1.ToString();
+            g2 = g1.ToString();
+            c2 = c1.ToString();
+
+            //MessageBox.Show(p2);
+            OdbcDataReader perfil = logic.InsertarPerfil(Txt_Cod.Text, Txt_puesto.Text, p2, s2, b2, es2, g2, c2, txt_extras.Text);
+            MessageBox.Show("Perfil Creado.");
+        }
+
+        private void Frm_MantPerfiles_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_editar_Click(object sender, EventArgs e)
+        {
+            //Conversion a numeros
+            bool p, s, b, es, g, c;
+            p = chc_primaria.Checked;
+            s = chc_secundaria.Checked;
+            b = chc_bachillerato.Checked;
+            es = chc_Estudiante.Checked;
+            g = chc_Graduado.Checked;
+            c = chc_Cursos.Checked;
+
+            string p2, s2, b2, es2, g2, c2;
+            int p1, s1, b1, es1, g1, c1;
+
+            p1 = Convert.ToInt32(p);
+            s1 = Convert.ToInt32(s);
+            b1 = Convert.ToInt32(b);
+            es1 = Convert.ToInt32(es);
+            g1 = Convert.ToInt32(g);
+            c1 = Convert.ToInt32(c);
+
+            p2 = p1.ToString();
+            s2 = s1.ToString();
+            b2 = b1.ToString();
+            es2 = es1.ToString();
+            g2 = g1.ToString();
+            c2 = c1.ToString();
+            OdbcDataReader perfil = logic.modificarPerfil(Txt_Cod.Text, Txt_puesto.Text, p2, s2, b2, es2, g2, c2, txt_extras.Text);
+            MessageBox.Show("Datos modificados correctamente.");
+        }
+
+        private void Btn_borrar_Click(object sender, EventArgs e)
+        {
+            OdbcDataReader perfil = logic.eliminarPerfil(Txt_Cod.Text);
+            MessageBox.Show("Eliminado Correctamentee.");
+        }
+    }
+}
