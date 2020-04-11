b2d68ea89fde9ff0a3a80464ca32d5578520a896
imogollonh <imogollonh@miumg.edu.gt>
2020-04-10 20:06:18 -0600

    Diseno factura proveedores

---
+++ Modulo SCM/SCM/Capa_Diseño_SCM/Procesos/Frm_FacturaProveedores.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Capa_Diseño_SCM
+{
+    public partial class Frm_FacturaProveedores : Form
+    {
+        public Frm_FacturaProveedores()
+        {
+            InitializeComponent();
+        }
+    }
+}
